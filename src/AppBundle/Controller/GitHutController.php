d7057aca3c2730a4614f7caaad7bc33c79ab984a
Laurynas <l.tumosa@lancaster.ac.uk>
2016-07-20 10:37:57 +0300

    added repos list

--- src/AppBundle/Controller/GitHutController.php
+++ src/AppBundle/Controller/GitHutController.php
@@ -29,7 +29,29 @@ class GitHutController extends Controller
                 'followers' => 1,
                 'following' => 2,
             ],
-        ]);
+            'repo_count' => 100,
+            'most_stars' => 50,
+            'repos' => [
+                [
+                    'url' => 'https://codereviewvideos.com',
+                    'name' => 'Code Review Videos',
+                    'description' => 'some repo description',
+                    'stargazers_count' => '999',
+                ],
+                [
+                    'url' => 'http://bbc.co.uk',
+                    'name' => 'The BBC',
+                    'description' => 'not a real repo',
+                    'stargazers_count' => '666',
+                ],
+                [
+                    'url' => 'http://google.co.uk',
+                    'name' => 'Google',
+                    'description' => 'another fake repo description',
+                    'stargazers_count' => '333',
+                ],
+                ],
+            ]);
     }
 
     /**
